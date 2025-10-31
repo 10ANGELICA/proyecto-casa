5e6501943f6b471e8bd185eaba22661a72da05f6
10ANGELICA <angelicafigueroahuaman8@gmail.com>
2025-10-31 00:22:51 -0500

    Actualizacion

--- src/app/carrusel/carrusel.ts
+++ src/app/carrusel/carrusel.ts
@@ -1,37 +1,67 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.html',
-  styleUrls: ['./carrusel.scss']  // estaba mal: era styleUrls (plural)
+  styleUrls: ['./carrusel.scss']
 })
 export class Carrusel implements OnInit, OnDestroy {
-
   images = [
-    'imagen01.png',
-    'imagen02.png',
-    'imagen04.png',
-    'imagen05.png',
-    'imagen06.png',
-    'imagen07.png',
+    {
+      src: 'imagen01.png',
+      title: 'Habitación Simple',
+      description: 'Perfecta para viajeros individuales, con cama cómoda, Wi-Fi gratuito y baño privado.'
+    },
+    {
+      src: 'imagen02.png',
+      title: 'Habitación Doble',
+      description: 'Ideal para parejas, con una cama matrimonial, decoración cálida y excelente iluminación natural.'
+    },
+    {
+      src: 'imagen04.png',
+      title: 'Habitación Familiar',
+      description: 'Espaciosa y acogedora, con capacidad hasta 4 personas, TV por cable y vista a la ciudad.'
+    },
+    {
+      src: 'imagen05.png',
+      title: 'Suite Premium',
+      description: 'Lujo y confort con balcón privado, escritorio de trabajo y mini bar.'
+    },
+    {
+      src: 'imagen06.png',
+      title: 'Habitación Económica',
+      description: 'Sencilla pero cómoda, ideal para estancias cortas con todas las comodidades básicas.'
+    }
   ];
 
   currentImageIndex = 0;
   intervalId: any;
 
   ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void {
+    this.stopCarousel();
+  }
+
+  startCarousel(): void {
     this.intervalId = setInterval(() => {
-      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-    }, 4000); // cambia cada 4 segundos
+      this.nextImage();
+    }, 4000);
   }
 
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+  stopCarousel(): void {
+    if (this.intervalId) clearInterval(this.intervalId);
   }
 
-  get currentImage() {
-    return this.images[this.currentImageIndex];
+  prevImage(): void {
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  nextImage(): void {
+    this.currentImageIndex =
+      (this.currentImageIndex + 1) % this.images.length;
   }
 }
